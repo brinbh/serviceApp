cb665a221ff0773c4153449391261c591151f4d1
brinbh <brittany.faye@outlook.com>
2017-10-30 16:19:48 -0600

    Creating document component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { DocumentItemComponent } from './documents/document-item/document-item.component';
+import { DocumentListComponent } from './documents/document-list/document-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentsComponent,
+    DocumentDetailComponent,
+    DocumentItemComponent,
+    DocumentListComponent
   ],
   imports: [
     BrowserModule
